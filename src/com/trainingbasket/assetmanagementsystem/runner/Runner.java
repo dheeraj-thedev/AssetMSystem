569e1d1ea63fe8d1fdcf7954464d62a0d7d2000f
Dheeraj Singh <bond.deejay@gmail.com>
2020-01-15 14:54:56 +0530

    Is first commit for initilisation of project for collaborate

---
+++ src/com/trainingbasket/assetmanagementsystem/runner/Runner.java
@@ -0,0 +1,12 @@
+package com.trainingbasket.assetmanagementsystem.runner;
+
+public class Runner {
+
+	public Runner() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Testing");
+	}
+}
